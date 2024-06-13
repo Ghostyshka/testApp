8f62d1f38e4ec40cb91cf33bd17802dbf49cb713
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-14 02:41:13 +0300

    created: Customer entity

---
+++ src/main/java/com/dev/testApp/entity/Customer.java
@@ -0,0 +1,38 @@
+package com.dev.testApp.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "customer")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "BIGINT")
+    private Long id;
+
+    @Column(name = "created", columnDefinition = "BIGINT")
+    private Long created;
+
+    @Column(name = "updated", columnDefinition = "BIGINT")
+    private Long updated;
+
+    @Column(name = "full_name", columnDefinition = "VARCHAR")
+    private String fullName;
+
+    @Column(name = "email", columnDefinition = "VARCHAR")
+    private String email;
+
+    @Column(name = "phone", nullable = true ,columnDefinition = "VARCHAR")
+    private String phone;
+
+    @Column(name = "is_active", columnDefinition = "BOOL")
+    private Boolean isActive = true;
+
+}
